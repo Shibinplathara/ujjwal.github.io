703ab6c4d16b43c5f3c4f77616c89c98e18b61f6
Shibinplathara <120809133+Shibinplathara@users.noreply.github.com>
2022-12-17 22:16:58 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var btn = document.getElementById('a')
+var pass = document.getElementById('pass')
+var lab = document.getElementById("label")
+
+
+function cheak(){
+	if (pass.value ==="rtx360-ujjwal") {
+		 console.log("Login Successfull")
+		 document.location.href="Ujjwal.html"
+	}
+}
